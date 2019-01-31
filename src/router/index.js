ececbc1a90be6fdba2a393c456811e0fb90a92dd
joseffffff <joseffff@protonmail.com>
2019-01-31 23:48:50 +0100

    Starting to generate dinamic grids

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
-import Router from 'vue-router'
+import Router from 'vue-router';
 
-import Main from '@/components/Main'
-import Stores from '@/components/stores/StoresComponent'
+import Main from '@/components/Main';
+import Stores from '@/components/stores/StoresComponent';
 import ConfigComponent from '@/components/ConfigComponent.vue';
-import Testing from '@/components/testing/RootTestingComponent'
+import Testing from '@/components/testing/RootTestingComponent';
+import TurnsScreenComponent from '@/components/turnsScreens/TurnsScreenComponent.vue';
 
 export default new Router({
-  mode: 'history',
+  //mode: 'history',
   routes: [
     {
       path: '/',
@@ -24,9 +25,14 @@ export default new Router({
       component: Testing
     },
     {
-      path: '/Config',
+      path: '/config',
       name: 'config',
       component: ConfigComponent
+    },
+    {
+      path: '/turns-screens',
+      name: 'turns-screen',
+      component: TurnsScreenComponent
     }
   ]
 })
