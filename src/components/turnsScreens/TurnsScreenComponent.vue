568b5d033277af29f5c9aaae830d690771acab9e
ZehaoChen <a15zehcheche@iam.cat>
2019-02-01 17:31:40 +0100

    crear els component row i column

--- src/components/turnsScreens/TurnsScreenComponent.vue
+++ src/components/turnsScreens/TurnsScreenComponent.vue
@@ -1,111 +1,104 @@
 <template>
-	<div>
-		<div class="row">
-			<div class="col-md-2 pt-4" style="border: 1px solid black">
-				<div class="mb-3">
-					<button class="btn btn-primary">
-						Add Row
-					</button>
-				</div>
-				<div class="mb-3">
-					<button class="btn btn-primary ">
-						Add Col
-					</button>
-				</div>
-			</div>
-			<div class="col-md-10 px-5 py-5" style="border: 1px solid black">
-				<div v-html="htmlGrid" class=""></div>
-			</div>
-		</div>
-	</div>
+  <div>
+    <div class="row">
+      <div class="col-md-2 pt-4" style="border: 1px solid black">
+        <div class="mb-3">
+          <button class="btn btn-primary">Add Row</button>
+        </div>
+        <div class="mb-3">
+          <button class="btn btn-primary">Add Col</button>
+        </div>
+      </div>
+      <div class="col-md-10 px-5 py-5" style="border: 1px solid black">
+        <div v-html="htmlGrid" class></div>
+
+        <div v-if="jsonConfig.rows">
+          <template v-for="row in jsonConfig.rows">
+            <row :key="row" :jsonConfig="row"/>
+          </template>
+        </div>
+        <div v-else-if="jsonConfig.cols">
+          <template v-for="column in jsonConfig.column">
+            <column :key="column" :jsonConfig="column"/>
+          </template>
+        </div>
+        <div v-else>{{jsonConfig.content}}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
+  data() {
+    return {
+      htmlGrid: "",
 
-	data() {
+      jsonConfig: {
+        rows: [
+          {
+            cols: [
+              {
+                width: 4,
+                content: "Aloha"
+              },
+              {
+                width: 4,
+                content: "Aloha"
+              },
+              {
+                width: 4,
+                rows: [
+                  {
+                    content: "Alo"
+                  },
+                  {
+                    content: "Alo"
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    };
+  },
 
-		return {
+  mounted() {
+    this.htmlGrid = this.generateGrid(this.jsonConfig);
+    console.log(this.htmlGrid);
+  },
 
-			htmlGrid: '',
+  methods: {
+    generateGrid(jsonConfig) {
+      let htmlCode = "";
 
-			jsonConfig: {
-				rows: [
-					{
-						cols: [
-							{
-								width: 4,
-								content: 'Aloha'
-							}, 
-							{
-								width: 4,
-								content: 'Aloha'
-							},
-							{
-								width: 4,
-								rows: [
-									{
-										content: 'Alo'
-									}, 
-									{
-										content: 'Alo'
-									}
-								]
-							}
-						]
-					}
-				],
-			}
-		}
-	},
-
-	mounted() {
-		this.htmlGrid = this.generateGrid(this.jsonConfig);
-		console.log(this.htmlGrid);
-	},
-
-	methods: {
-
-		generateGrid(jsonConfig) {
-
-			let htmlCode = '';
-
-			if (jsonConfig.rows) {
-
-				for (let i = 0; i < jsonConfig.rows.length; i++) {
-					htmlCode += `<div class="row px-3 py-3 mx-0" style="border: 1px solid black">
+      if (jsonConfig.rows) {
+        for (let i = 0; i < jsonConfig.rows.length; i++) {
+          htmlCode += `<div class="row px-3 py-3 mx-0" style="border: 1px solid black">
 													${this.generateGrid(jsonConfig.rows[i])}
 												</div>`;
-				}
+        }
 
-				return htmlCode;
-
-			} else if (jsonConfig.cols) {
-
-				for (let i = 0; i < jsonConfig.cols.length; i++) {
-					htmlCode += `<div class="col-md-${jsonConfig.cols[i].width} px-3 py-3 mx-0" style="border: 1px solid black">
+        return htmlCode;
+      } else if (jsonConfig.cols) {
+        for (let i = 0; i < jsonConfig.cols.length; i++) {
+          htmlCode += `<div class="col-md-${
+            jsonConfig.cols[i].width
+          } px-3 py-3 mx-0" style="border: 1px solid black">
 													${this.generateGrid(jsonConfig.cols[i])}
 												</div>`;
-				}
+        }
 
-				return htmlCode;
+        return htmlCode;
+      } else {
+        return jsonConfig.content;
+      }
+    },
 
-			} else {
-				
-				return jsonConfig.content;
-
-			}
-		},
-
-		addCol() {
-
-		},
-		addRow() {
-
-		}
-	}
-
-}
+    addCol() {},
+    addRow() {}
+  }
+};
 </script>
 
---
+++ src/components/turnsScreens/row.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row px-3 py-3 mx-0" style="border: 1px solid black" >
+        <div v-if="jsonConfig.rows">
+			  <template v-for="row in jsonConfig.rows" >
+					<row  :key="row" :jsonConfig="row" />
+			  </template>	
+		</div>
+        <div v-else-if="jsonConfig.cols">
+			<template v-for="column in jsonConfig.column" >
+					<column  :key="column" :jsonConfig="column" />
+			  </template>
+		</div>
+        <div v-else> {{jsonConfig.content}}</div>
+   </div>
+</template>
+
+<script>
+export default {
+  props: {
+    jsonConfig: Object
+  },
+  data() {
+
+  },
+
+  mounted() {
+   
+  },
+
+  methods: {
+  }
+};
+</script>
+
