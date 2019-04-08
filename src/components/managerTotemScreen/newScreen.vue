bd5f4556b6753a866307ba40e369a0e6f231af05
ZehaoChen <a15zehcheche@iam.cat>
2019-04-08 21:42:34 +0200

    millorar la creacio del pantalla de totem

--- src/components/managerTotemScreen/newScreen.vue
+++ src/components/managerTotemScreen/newScreen.vue
@@ -1,6 +1,53 @@
 <template>
   <div>
-    <div v-if="templateSelect" class="form">
+    <div v-if="templateSelect && model != 'edit'" class="form">
+      <h1>Plantilla selectionado : {{templateSelect.NAME}}</h1>
+      <button @click="crear()" class="btn btn-primary mb-2">Crear</button>
+    </div>
+
+    <div v-if="model == 'edit'" class="main_edit">
+      <div class="content">
+        <div class="info">
+          <div class="form-group row">
+            <h1 class="col-sm-2 col-form-label">Nom</h1>
+            <div class="col-sm-10">
+              <input
+                required="required"
+                v-model="newName"
+                type="text"
+                class="form-control"
+                placeholder="nuevo nombre"
+              >
+            </div>
+          </div>
+          <div class="form-group row">
+            <h1 class="col-sm-2 col-form-label">Descripcion</h1>
+            <div class="col-sm-10">
+              <input
+                required="required"
+                v-model="description"
+                type="text"
+                class="form-control"
+                placeholder
+              >
+            </div>
+          </div>
+        </div>
+        <div class="template">
+          <div id="option"></div>
+        </div>
+      </div>
+      <div class="option">
+        <div class="cancel">
+          <button @click="$emit('input', 'home');" type="button" class="btn btn-danger">Cancelar</button>
+        </div>
+        <div class="save">
+          <button @click="save()" type="button" class="btn btn-primary">Guardar</button>
+        </div>
+      </div>
+    </div>
+
+    <!--div v-if="templateSelect" class="form">
       <div class="form-group row">
         <label class="col-sm-5 col-form-label">Plantilla selectionado</label>
         <div class="col-sm-5">
@@ -32,9 +79,9 @@
         </div>
         <button @click="save()" class="btn btn-primary mb-2">Guardar</button>
       </div>
-    </div>
+    </div-->
 
-    <div class="templateContent">
+    <div v-else-if="model === 'select'" class="templateContent">
       <div
         class="template"
         @click="select(index)"
@@ -47,11 +94,11 @@
           </div>
         </header>
         <div class="content">
-            <screen-demo-component :jsonConfig="JSON.parse(template.LAYOUT)"/>
+          <screen-demo-component :jsonConfig="JSON.parse(template.LAYOUT)"/>
         </div>
       </div>
     </div>
-    <div class="cancel">
+    <div v-if="model != 'edit'" class="cancel">
       <button @click="$emit('input', 'home');" type="button" class="btn btn-danger">Cancelar</button>
     </div>
   </div>
@@ -62,6 +109,11 @@ import axios from "axios";
 import urls from "../../api/config.js";
 import demoScreen from "./screenDemo.vue";
 
+import swal from "sweetalert2";
+global.jQuery = require("jQuery");
+var $ = global.jQuery;
+window.$ = $;
+
 export default {
   components: {
     "screen-demo-component": demoScreen
@@ -71,7 +123,16 @@ export default {
       templates: null,
       templateSelect: null,
       newName: null,
-      description: null
+      description: null,
+
+      model: "select",
+      StoreListHtmlCode: "",
+      stores: [],
+      sections: [0],
+      storeSelect: [],
+      selectPositionCountID: 0,
+      layoutPositionCountID: 0,
+      newScreenLayout: ""
     };
   },
   mounted: function() {
@@ -96,25 +157,49 @@ export default {
     },
     save() {
       if (this.templateSelect && this.newName && this.description) {
-        console.log(this.newName);
-        console.log(this.description);
+        console.log("save");
+        let storeSelect = [];
+        $(".selecte").each(function(i) {
+          storeSelect[$(this).attr("selectId")] = $(this)
+            .children("option:selected")
+            .val();
+        });
+        this.storeSelect = storeSelect;
+        console.log(this.storeSelect);
+        this.newScreenLayout = "{";
+        this.generateNewScreenLayout(JSON.parse(this.templateSelect.LAYOUT));
+        this.newScreenLayout += "}";
+        this.layoutPositionCountID = 0;
+
+        console.log("Name:" + this.newName);
+        console.log("Descripcion:" + this.description);
+        console.log(this.newScreenLayout);
+
         let reference = this;
+
         const url = urls.host + urls.routes.apiPrefix + urls.routes.layouts;
         axios
           .post(url, {
             NAME: this.newName,
-            LAYOUT: this.templateSelect.LAYOUT,
+            LAYOUT: this.newScreenLayout,
             DESCRIPTION: this.description,
             TYPE: "TOTEM"
           })
           .then(function(response) {
             console.log(response);
             reference.templateSelect = null;
+            reference.model = "select";
           })
           .catch(function(error) {
             console.log(error);
           });
+      } else {
+        swal("Aviso!", "Falten el nombre o el descripcions!");
       }
+    },
+    crear() {
+      this.getStores();
+      this.model = "edit";
     },
     select(index) {
       console.log(index);
@@ -122,6 +207,126 @@ export default {
     },
     setmode(mode) {
       this.mode = mode;
+    },
+    getStores() {
+      const url = urls.host + urls.routes.prefix + urls.routes.stores;
+      console.log(url);
+      var reference = this;
+      axios
+        .get(url)
+        .then(res => {
+          reference.stores = res.data;
+          this.createScreenLayout();
+          console.log(res.data);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    },
+    createScreenLayout() {
+      this.StoreListHtmlCode = this.generateGrid(
+        JSON.parse(this.templateSelect.LAYOUT)
+      );
+      console.log(this.templateSelect.LAYOUT);
+      $("#option").html(this.StoreListHtmlCode);
+    },
+    generateGrid(jsonConfig) {
+      this.StoreListHtmlCode = "";
+      if (jsonConfig.rows) {
+        for (let i = 0; i < jsonConfig.rows.length; i++) {
+          this.StoreListHtmlCode += `<div class="row px-3 py-3 mx-0" style="height: ${
+            jsonConfig.rows[i].height
+          }%;border: 1px solid black">
+													${this.generateGrid(jsonConfig.rows[i])}
+												</div>`;
+        }
+
+        return this.StoreListHtmlCode;
+      } else if (jsonConfig.cols) {
+        for (let i = 0; i < jsonConfig.cols.length; i++) {
+          this.StoreListHtmlCode += `<div class="col-md-${
+            jsonConfig.cols[i].width
+          } px-3 py-3 mx-0 " style=" height: ${
+            jsonConfig.cols[i].height
+          }%; border: 1px solid black">
+													${this.generateGrid(jsonConfig.cols[i])}
+												</div>`;
+        }
+        return this.StoreListHtmlCode;
+      } else {
+        return this.crearStoreList(jsonConfig.id);
+      }
+      //console.log(StoreListHtmlCode);
+
+      this.StoreListHtmlCode = StoreListHtmlCode;
+    },
+    crearStoreList(id) {
+      this.StoreListHtmlCode +=
+        `<select selectId="${
+          this.selectPositionCountID
+        }" class="form-control selecte` + '">';
+      for (let i = 0; i < this.stores.length; i++) {
+        if (i + 1 == id) {
+          this.StoreListHtmlCode +=
+            '<option selected value="' +
+            `${i + 1}` +
+            '">' +
+            this.stores[i].name +
+            "</option>";
+        } else {
+          this.StoreListHtmlCode +=
+            '<option value="' +
+            `${i + 1}` +
+            '">' +
+            this.stores[i].name +
+            "</option>";
+        }
+      }
+      this.StoreListHtmlCode += "</select>";
+      this.selectPositionCountID++;
+      return this.StoreListHtmlCode;
+      //console.log(this.StoreListHtmlCode);
+    },
+    generateNewScreenLayout(newLayout) {
+      if (newLayout.rows) {
+        this.newScreenLayout += `"height":"${newLayout.height}","width":${
+          newLayout.width
+        },"rows":[`;
+        for (let i = 0; i < newLayout.rows.length; i++) {
+          this.newScreenLayout += "{";
+
+          this.generateNewScreenLayout(newLayout.rows[i]);
+          if (i == newLayout.rows.length - 1) {
+            this.newScreenLayout += "}";
+          } else {
+            this.newScreenLayout += "},";
+          }
+        }
+        this.newScreenLayout += "]";
+        return this.newScreenLayout;
+      } else if (newLayout.cols) {
+        this.newScreenLayout += `"height":"${newLayout.height}","cols":[`;
+        for (let i = 0; i < newLayout.cols.length; i++) {
+          this.newScreenLayout += "{";
+
+          this.generateNewScreenLayout(newLayout.cols[i]);
+          if (i == newLayout.cols.length - 1) {
+            this.newScreenLayout += "}";
+          } else {
+            this.newScreenLayout += "},";
+          }
+        }
+        this.newScreenLayout += "]";
+        return this.newScreenLayout;
+      } else {
+        this.newScreenLayout += `"height":"${newLayout.height}",`;
+        this.newScreenLayout += `"width":${newLayout.width},`;
+        this.newScreenLayout += `"id":${
+          this.storeSelect[this.layoutPositionCountID]
+        }`;
+        this.layoutPositionCountID++;
+        return this.newScreenLayout;
+      }
     }
   }
 };
@@ -161,5 +366,48 @@ export default {
 .template .content {
   height: 85%;
   margin: 0%;
+}
+
+/* edit */
+.main_edit {
+  height: 80%;
+}
+.main_edit .content {
+  height: 700px;
+  width: 80%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+}
+.info {
+  width: 100%;
+  font-size: 2rem;
+  background-color: #999;
+  margin: 1% 0;
+  border-radius: 5px;
+}
+
+.info .col-sm-10 {
+  display: flex;
+  align-items: center;
+}
+
+.main_edit .content .template {
+  background-color: #aaa;
+  height: 90%;
+  width: 100%;
+  padding: 0;
+}
+
+.main_edit .option {
+  height: 20%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.template #option {
+  height: 100%;
 }
 </style>
